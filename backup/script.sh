74d24fbc805d933fd9f450d97cc4aa6437588a69
Simon Persson <simon20396@gmail.com>
2025-05-05 11:23:40 +0200

    Cleanup

--- backup/script.sh
+++ backup/script.sh
@@ -1,20 +1,12 @@
 #!/bin/sh
-
-set -x
-
-TIMESTAMP=$(date +"%Y-%m-%d-%H-%M")
-BACKUP_DIR="/mnt/pg_backup/$TIMESTAMP"  
-
-echo "Creating backup directory at $BACKUP_DIR + "
+TIMESTAMP=$(date +"%Y-%m-%d")
+BACKUP_DIR="/mnt/backups/$DATABASE_NAME $TIMESTAMP"  
 mkdir -p "$BACKUP_DIR"
 
-# Dump PostgreSQL database
-echo "Backing up PostgreSQL to $BACKUP_DIR/postgres.dump"
-echo "Database username: $DATABASE_USERNAME Database name: $DATABASE_NAME"
+# Make pg_dump
 PGPASSWORD="$DATABASE_PASSWORD" pg_dump -h postgres-server -p 5432 -U "$DATABASE_USERNAME" -d "$DATABASE_NAME" -F c -b -v -f "$BACKUP_DIR/postgres.dump"
 
 # Copy Strapi config, src, and uploads
-echo "Copying Strapi config, src, and uploads"
 cp -r /mnt/strapi_config "$BACKUP_DIR/config"
 cp -r /mnt/strapi_src "$BACKUP_DIR/src"
 cp -r /mnt/strapi_uploads "$BACKUP_DIR/uploads"
