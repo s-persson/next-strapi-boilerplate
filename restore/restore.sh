d19813eb3e3abdc92b3b1a2290913697b8b620b0
Simon Persson <simon20396@gmail.com>
2025-05-07 21:34:45 +0200

    Debugging of mounted config, src, uploads folders

--- restore/restore.sh
+++ restore/restore.sh
@@ -3,6 +3,20 @@ if [ -z "$RESTORE_PATH" ]; then
   echo "❌ RESTORE_PATH not set"
   exit 1
 fi
+echo "🔍 Before rsync:"
+ls -l /mnt/strapi_src/
+
+echo "Stopping all connections"
+PGPASSWORD="$DATABASE_PASSWORD" psql -h postgres-server -U "$DATABASE_USERNAME" -d postgres -c "
+SELECT pg_terminate_backend(pg_stat_activity.pid)
+FROM pg_stat_activity
+WHERE pg_stat_activity.datname = '$DATABASE_NAME'
+  AND pid <> pg_backend_pid();
+"
+
+echo "Removing and recreating database"
+PGPASSWORD="$DATABASE_PASSWORD" psql -h postgres-server -U "$DATABASE_USERNAME" -d postgres -c "DROP DATABASE IF EXISTS \"$DATABASE_NAME\";"
+PGPASSWORD="$DATABASE_PASSWORD" psql -h postgres-server -U "$DATABASE_USERNAME" -d postgres -c "CREATE DATABASE \"$DATABASE_NAME\";"
 
 echo "🔁 Restoring from $RESTORE_PATH"
   
@@ -14,5 +28,8 @@ echo "🔁 Restoring Strapi files..."
 rsync -a "$RESTORE_PATH/config/" /mnt/strapi_config/
 rsync -a "$RESTORE_PATH/src/" /mnt/strapi_src/
 rsync -a "$RESTORE_PATH/uploads/" /mnt/strapi_uploads/
+
+echo "🔍 After rsync:"
+ls -l /mnt/strapi_src/
 
 echo "✅ Restore completed!"
