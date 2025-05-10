b82a1cf6b821edebb9862f1ae7305a8152408003
Simon Persson <simon20396@gmail.com>
2025-05-10 23:09:19 +0200

    WIP Rsyncing backup to external heztner storage box

--- backup/backup.sh
+++ backup/backup.sh
@@ -7,8 +7,11 @@ mkdir -p "$BACKUP_DIR"
 PGPASSWORD="$DATABASE_PASSWORD" pg_dump -h postgres-server -p 5432 -U "$DATABASE_USERNAME" -d "$DATABASE_NAME" -F c -b -v -f "$BACKUP_DIR/postgres.dump"
 
 # Copy Strapi config, src, and uploads
-cp -r /mnt/strapi_config "$BACKUP_DIR/config"
-cp -r /mnt/strapi_src "$BACKUP_DIR/src"
-cp -r /mnt/strapi_uploads "$BACKUP_DIR/uploads"
+rsync -r /mnt/strapi_config "$BACKUP_DIR/config"
+rsync -r /mnt/strapi_src "$BACKUP_DIR/src"
+rsync -r /mnt/strapi_uploads "$BACKUP_DIR/uploads"
+
+echo "Rsyncing backup to hetzner storage box"
+rsync -avz -e "ssh -p23" "$BACKUP_DIR/" u420764@u420764.your-storagebox.de:/home/u420764/backups/"$DATABASE_NAME/$TIMESTAMP"/
 
 echo "✅ Backup completed successfully!"
